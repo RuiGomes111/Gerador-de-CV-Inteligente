a54196b85d547dbb0228e3ba4f11a47f6147ce58
Rui Gomes <ruisoft01@gmail.com>
2025-08-27 21:40:35 +0100

    features/configurações iniciais react + vite + tailwind

---
+++ gerador_cv/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-900 ">
+      <h1 className="text-4xl font-bold text-blue-600">🚀 Gerador de Currículo Inteligente com
+Preview em Tempo Real !</h1>
+
+      
+    </div>
+  )
+}
+
+export default App
