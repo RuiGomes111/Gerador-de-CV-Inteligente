26a04a2465815b4f42601f2b9e13e7f9e4ea57d0
manu-melo <mn.manu822@gmail.com>
2025-08-30 16:06:57 -0300

    limpando App.tsx e importando MainLayout

--- gerador_cv/src/App.tsx
+++ gerador_cv/src/App.tsx
@@ -1,32 +1,10 @@
 import { CurriculumProvider } from "./context/CurriculumContext";
+import { MainLayout } from "./components/MainLayout";
 
 function App() {
   return (
     <CurriculumProvider>
-      <div className="h-screen grid grid-cols-2 bg-gray-900">
-        {/* Coluna esquerda - Formulário */}
-        <div className="bg-[#272727] overflow-y-auto">
-          <div className="p-6">
-            <h1 className="text-white text-2xl font-bold mb-6">
-              Gerador de CV
-            </h1>
-            {/* Conteúdo do formulário será adicionado aqui */}
-          </div>
-        </div>
-
-        {/* Separador visual */}
-        <div className="w-px bg-gray-600 absolute left-1/2 top-0 h-full"></div>
-
-        {/* Coluna direita - Preview do CV */}
-        <div className="bg-[#EFD09E] overflow-y-auto">
-          <div className="p-6">
-            <h2 className="text-gray-800 text-2xl font-bold mb-6">
-              Preview do CV
-            </h2>
-            {/* Preview do CV será adicionado aqui */}
-          </div>
-        </div>
-      </div>
+      <MainLayout />
     </CurriculumProvider>
   );
 }
